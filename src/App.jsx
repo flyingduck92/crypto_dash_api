4fe928a3eff82fa9bafc1ecf5c8376353d2d0602
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-09-05 17:10:49 +0700

    Add limit selector

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import CoinCard from './components/CoinCard'
+import LimitSelector from './components/LimitSelector'
 
 const API_URL = import.meta.env.VITE_API_URL
 
@@ -7,11 +8,12 @@ const App = () => {
   const [coins, setCoins] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const [limit, setLimit] = useState(10)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch(`${API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`)
+        const res = await fetch(`${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`)
         if (!res.ok) throw Error('Failed to fetch data')
         const data = await res.json()
         console.log(data)
@@ -23,13 +25,15 @@ const App = () => {
       }
     }
     fetchData()
-  }, [])
+  }, [limit])
 
   return (
     <div>
       <h1>🚀 Crypto Dash</h1>
       {loading && <p>Loading...</p>}
       {error && <div className="error">{error}</div>}
+
+      <LimitSelector limit={limit} onLimitChange={setLimit} />
 
       {
         !loading && !error && (
