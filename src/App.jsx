a232b548b8ac962c3620e88702f714be2ddc7c64
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-09-05 17:01:51 +0700

    Add coincard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
+import { useEffect, useState } from 'react'
+import CoinCard from './components/CoinCard'
+
+const API_URL = import.meta.env.VITE_API_URL
+
 const App = () => {
+  const [coins, setCoins] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`${API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`)
+        if (!res.ok) throw Error('Failed to fetch data')
+        const data = await res.json()
+        console.log(data)
+        setCoins(data)
+      } catch (err) {
+        setError(err.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <div>
       <h1>🚀 Crypto Dash</h1>
+      {loading && <p>Loading...</p>}
+      {error && <div className="error">{error}</div>}
+
+      {
+        !loading && !error && (
+          <main className="grid">
+            {coins.map(coin => (
+              <CoinCard key={coin.id} coin={coin} />
+            ))}
+          </main>
+        )
+      }
+
     </div>
   )
 }
