01159595ddfdd76851cae79acda488cde26b59ba
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-09-06 08:30:35 +0700

    Add react-router + header + pages + loading spinner

---
+++ src/pages/about.jsx
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+  return (
+    <div className="about">
+      <h1>About Crypto Dash</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur consectetur iure repudiandae sequi quidem quisquam, molestiae animi obcaecati praesentium tenetur consequuntur expedita officia quae porro perspiciatis quasi autem quas rem?</p>
+    </div>
+  )
+}
+
+export default AboutPage
